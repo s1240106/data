c850a64efe778bfd6d6bdf2837477cd59275a368
s1240106 <s1240106@std3dc32>
2018-07-02 14:42:13 +0900

    Won/lost message modified

--- randdice.c
+++ randdice.c
@@ -16,8 +16,8 @@ srand(time(NULL));
   total=die1+die2;
   printf("Total value: %d\n",total);
    if(total>7)
-    printf("You won!\n");
+     printf("%s won!\n",name);
   else
-    printf("You lost\n");
+    printf("%s lost\n",name);
     return 0;
 }
