78ae00e31d90d0ec5015bea66bf18f66d42b4263
s1240106 <s1240106@std3dc32>
2018-07-02 14:18:13 +0900

    Initial revision

---
+++ randdice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+ int die1,die2,total;
+srand(time(NULL));
+  printf("Rolling the dice...\n");
+  die1=rand()%6+1;
+  die2=rand()%6+1;
+  printf("Die 1: %d\n",die1);
+  printf("Die 2: %d\n",die2);
+
+  total=die1+die2;
+  printf("Total value: %d\n",total);
+    return 0;
+}
